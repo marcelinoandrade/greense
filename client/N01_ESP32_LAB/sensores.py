69e563aa186da3747147e01343d502c95aff69a2
Marcelino Andrade <mrclnndrd@gmail.com>
2025-03-03 20:41:38 -0300

    Descrição das mudanças

--- client/N01_ESP32_LAB/sensores.py
+++ client/N01_ESP32_LAB/sensores.py
@@ -3,17 +3,41 @@ import random
 class SensorManager:
     def __init__(self):
         pass
-
+    
     def read_sensors(self):
-        temperatura = round(random.uniform(20, 35), 2)
-        umidade = round(random.uniform(40, 80), 2)
-        umidade_solo = round(random.uniform(10, 90), 2)
-        luz = round(random.uniform(100, 1000), 2)
+        # Sensores ambientais na estufa
+        temp = round(random.uniform(18, 30), 2)  # °C
+        umid = round(random.uniform(50, 90), 2)  # %
+        co2 = round(random.uniform(300, 1000), 2)  # ppm (partes por milhão)
+        luz = round(random.uniform(100, 2000), 2)  # Lux
+
+        # Sensores da solução nutritiva (reservatório interno na estufa)
+        agua_min = round(random.uniform(0, 60), 2)  # Boia inferior
+        agua_max = round(random.uniform(agua_min, 100), 2)  # Boia superior
+        temp_reserv_int = round(random.uniform(15, 25), 2)  # Temperatura do reservatório interno
+
+        # Sensores da solução nutritiva (reservatório externo)
+        ph = round(random.uniform(5.5, 6.5), 2)  # pH ideal para hidroponia
+        ec = round(random.uniform(1.0, 2.5), 2)  # mS/cm (Condutividade elétrica)
+        temp_reserv_ext = round(random.uniform(15, 25), 2)  # Temperatura do reservatório externo
 
         dados = {
-            "temperatura": temperatura,
-            "umidade": umidade,
-            "umidade_solo": umidade_solo,
-            "luz": luz
+            # Sensores ambientais da estufa
+            "temp": temp,
+            "umid": umid,
+            "co2": co2,
+            "luz": luz,
+
+            # Sensores da solução nutritiva (reservatório interno)
+            "agua_min": agua_min,
+            "agua_max": agua_max,
+            "temp_reserv_int": temp_reserv_int,
+
+            # Sensores da solução nutritiva (reservatório externo)
+            "ph": ph,
+            "ec": ec,
+            "temp_reserv_ext": temp_reserv_ext,
         }
+        
+        
         return dados
--- server/N01_RASP4_LAB/app.py
+++ server/N01_RASP4_LAB/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    grafana_url = "http://localhost:3000/d-solo/deefsj9h9fqbkf/estufa-01?orgId=1&from=1740952234728&to=1740952534728&timezone=browser&refresh=auto&panelId=3&__feature.dashboardSceneSolo"
+    grafana_url = "http://localhost:3000/d-solo/deefsj9h9fqbkf/estufa-01?orgId=1&from=now-5m&to=now&timezone=browser&refresh=5s&panelId=3"
+    grafana_url = "http://localhost:3000/public-dashboards/4d9136643ade492fbe122c38ab6ae5be"
     return render_template('grafana_dashboard.html', grafana_url=grafana_url)
 
 if __name__ == '__main__':
