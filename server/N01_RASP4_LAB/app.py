646effaf7ebc9d9ba3945a645e16e356cb329b4c
Marcelino Andrade <mrclnndrd@gmail.com>
2025-03-05 08:18:37 -0300

    Atualizações

--- server/N01_RASP4_LAB/app.py
+++ server/N01_RASP4_LAB/app.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# URLs dos Dashboards no Grafana
+GRAFANA_ESTUFA_1 = "http://127.0.0.1:3000/d/deefsj9h9fqbkf/estufa-01?orgId=1&from=now-5m&to=now&timezone=browser&refresh=auto&kiosk"
+GRAFANA_ESTUFA_2 = "http://127.0.0.1:3000/d/deefsj9h9fqbkf/estufa-02?orgId=1&from=now-5m&to=now&timezone=browser&refresh=auto&kiosk"
+
 @app.route('/')
 def home():
-    grafana_url = "http://localhost:3000/d-solo/deefsj9h9fqbkf/estufa-01?orgId=1&from=now-5m&to=now&timezone=browser&refresh=5s&panelId=3"
-    grafana_url = "http://localhost:3000/public-dashboards/4d9136643ade492fbe122c38ab6ae5be"
-    return render_template('grafana_dashboard.html', grafana_url=grafana_url)
+    return render_template('index_greense.html', grafana_estufa_1=GRAFANA_ESTUFA_1, grafana_estufa_2=GRAFANA_ESTUFA_2)
+
+@app.route('/estufa1')
+def estufa1():
+    return render_template('grafana_dashboard.html', grafana_url=GRAFANA_ESTUFA_1)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
