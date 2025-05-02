92a0dc8d2a8374b8bc7350a108186641c01f8006
Marcelino Andrade <mrclnndrd@gmail.com>
2025-05-02 14:32:12 -0300

    atualização rápida

--- server/N01_RASP4_LAB/server01Full.py
+++ server/N01_RASP4_LAB/server01Full.py
@@ -79,6 +79,29 @@ def on_message(client, userdata, msg):
                 }
             ]
 
+        elif msg.topic == "estufa/maturar":
+            dispositivo = "Estufa_Maturar"
+            json_body = [
+                {
+                    "measurement": "sensores",
+                    "tags": {"dispositivo": dispositivo},
+                    "fields": {
+                        "temp": data.get("temp", 0),
+                        "umid": data.get("umid", 0),
+                        "co2": data.get("co2", 0),
+                        "luz": data.get("luz", 0),
+                        "agua_min": float(data.get("agua_min", 0)),
+                        "agua_max": float(data.get("agua_max", 0)),
+                        "temp_reserv_int": data.get("temp_reserv_int", 0),
+                        "ph": data.get("ph", 0),
+                        "ec": data.get("ec", 0),
+                        "temp_reserv_ext": data.get("temp_reserv_ext", 0)
+                    #    "umid_solo_pct": data.get("umid_solo_pct", 0),  # <-- novo campo
+                    #    "umid_solo_raw": data.get("umid_solo_raw", 0)   # <-- opcional
+                    }
+                }
+            ]
+
         elif msg.topic == "estufa3/esp32":
             dispositivo = "ESP32_E3"
             json_body = [
@@ -100,27 +123,6 @@ def on_message(client, userdata, msg):
                 }
             ]
 
-        elif msg.topic == "estufa4/uno":
-            dispositivo = "UNO_E4"
-            json_body = [
-                {
-                    "measurement": "sensores",
-                    "tags": {"dispositivo": dispositivo},
-                    "fields": {
-                        "painel": float(data.get("painel", 0)),
-                        "exaustor": float(data.get("exaustor", 0)),
-                        "temp": float(data.get("temp", 0)),
-                        "umid": float(data.get("umid", 0)),
-                        "temp_solo": float(data.get("temp_solo", 0)),
-                        "umid_solo": float(data.get("umid_solo", 0)),
-                        "boia_baixa": float(data.get("boia_baixa", 0)),
-                        "boia_alta": float(data.get("boia_alta", 0)),
-                        "bomba_baixa": float(data.get("bomba_baixa", 0)),
-                        "bomba_alta": float(data.get("bomba_alta", 0))
-                    }
-                }
-            ]
-
         else:
             print(f"⚠️ Tópico desconhecido: {msg.topic}")
             return
@@ -137,8 +139,8 @@ def start_mqtt():
     client_mqtt.on_message = on_message
     client_mqtt.connect("localhost", 1883, 60)
     client_mqtt.subscribe("estufa/germinar")
+    client_mqtt.subscribe("estufa/maturar")
     client_mqtt.subscribe("estufa3/esp32")
-    client_mqtt.subscribe("estufa4/uno")
 
     print("Servidor MQTT rodando... Aguardando mensagens.")
     client_mqtt.loop_forever()
