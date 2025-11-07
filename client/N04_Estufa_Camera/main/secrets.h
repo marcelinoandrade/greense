3406caceabb8367b912147f5b9fa901feb8b9d32
raspLino <mrclnndrd@gmail.com>
2025-11-07 16:06:36 -0300

    atualização rápida

---
+++ client/N04_Estufa_Camera/main/secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID       "greense"
+#define WIFI_PASS       "greense@3141"
+#define CAMERA_UPLOAD_URL "https://camera02.greense.com.br/upload"
+
+
+#endif // SECRETS_H
